b090f55766cd8a39eed6ce835ffa014b2120bf4d
Administrator <zahrakazemy@gmail.com>
2022-01-20 16:35:13 +0330

    change the spring fox configuration

--- src/main/java/com/kazemi/challenge/challenge/SpringFoxConfig.java
+++ src/main/java/com/kazemi/challenge/challenge/SpringFoxConfig.java
@@ -4,19 +4,37 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Collections;
+
 @Configuration
 @EnableSwagger2
 public class SpringFoxConfig {
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "USER SERVICE CODE CHALLENGE",
+                "Some custom description of API.",
+                "API TOS",
+                "",
+                new Contact("zahra kazemi", null, "zahrakazemy@gmail.com"),
+                "",
+                "",
+                Collections.emptyList());
+    }
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage( "com.kazemi"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .useDefaultResponseMessages(false);
     }
+
 }
